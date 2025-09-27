67978c8f571d8ef8ca4521bd4afa87a5cad3f8c8
sts19813 <sts19813@gmail.com>
2025-09-26 18:31:36 -0600

    gtm instalacion

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -17,4 +17,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/style.css">
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5RT6K3RZ');</script>
+    <!-- End Google Tag Manager -->
 </head>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,9 @@
 <!-- Header -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RT6K3RZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <nav class="navbar fixed-top bg-white shadow-sm px-3 py-2 z-3">
     <div class="container-fluid d-flex justify-content-between align-items-center position-relative">
 
